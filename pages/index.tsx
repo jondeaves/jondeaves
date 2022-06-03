e6dea73ba0e41f77e41c91acdda0886e8b2dfdde
Jon Deaves <hello@jondeaves.me>
2022-06-03 15:45:10 +0100

    feat: begin implementing next.js

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Jon Deaves | Software Developer</title>
+        <meta
+          name="description"
+          content="Jon Deaves is a Software Engineer who makes things, usually with code."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Jon Deaves</h1>
+        <h2>Software Developer and hobby enthusiast</h2>
+
+        <p className={styles.description}>
+          An experienced developer specialising in interactive web applications.
+          I take pride in creating modern, responsive and progressive user
+          experiences.
+        </p>
+      </main>
+    </>
+  );
+};
+
+export default Home;
