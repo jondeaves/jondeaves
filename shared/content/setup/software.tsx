358d581729528d05892cb1548b2762e0510731dc
Jon Deaves <hi@jondeaves.me>
2022-12-28 02:01:40 -0500

    refactor: clean-up setup page

---
+++ shared/content/setup/software.tsx
@@ -0,0 +1,14 @@
+import Accordion from "../../../components/Accordion";
+
+const SoftwareSetup = () => {
+  return (
+    <Accordion summary="Software" tag="software">
+      <p>
+        These are the programs and tools I regularly use while working on both
+        my Mac and PC.
+      </p>
+    </Accordion>
+  );
+};
+
+export default SoftwareSetup;
---
+++ shared/content/setup/technologies.tsx
@@ -0,0 +1,14 @@
+import Accordion from "../../../components/Accordion";
+
+const TechnologySetup = () => {
+  return (
+    <Accordion summary="Technologies" tag="technologies">
+      <p>
+        Some of the services and other resources I make use of that are not
+        strictly software packages.
+      </p>
+    </Accordion>
+  );
+};
+
+export default TechnologySetup;
